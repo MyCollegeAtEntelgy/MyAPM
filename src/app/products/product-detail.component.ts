0c6ca72cf23ec62bd3dac284d27323455561cc99
Adolfo@Entelgy <adolfo.garcia@entelgy.com>
2018-09-30 17:31:31 +0200

    20180930 -  Añadido routing

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from "@angular/router";    // No requierie registro, pues es parte del modulo Router
+
 import { IProduct } from './product';
 
 @Component({
@@ -10,9 +12,29 @@ export class ProductDetailComponent implements OnInit {
   pageTitle: string ='Product Detail';
   product: IProduct
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router
+    ) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+
+    this.pageTitle +=`:${id}`;
+    this.product = {
+      "productId": 5,
+      "productName": "Hammer",
+      "productCode": "TBX-0048",
+      "releaseDate": "May 21, 2016",
+      "description": "Curved claw steel hammer",
+      "price": 8.9,
+      "starRating": 4.8,
+      "imageUrl": "https://openclipart.org/image/300px/svg_to_png/73/rejon_Hammer.png"
+    }
+  }
+
+
+  onBack(): void {
+    this.router.navigate(['/products'])
   }
 
 }
